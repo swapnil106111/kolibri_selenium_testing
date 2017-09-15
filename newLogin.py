7dc3c9e8f8d7dbfcf314b3dcebeb75f1d43f6343
swapnil106111 <swap106111@gmail.com>
2017-09-15 12:37:58 +0530

    playing videos and giving exercise

--- newLogin.py
+++ newLogin.py
@@ -8,6 +8,11 @@ import sys
 import os
 from tkinter import *
 
+user = [{"admin": "password"},
+		{"pm": "sc"},
+		{"coach": "sc"},
+		{"learner":"sc"},
+		{"student": "sc"}]
 
 class KolibriTesting(unittest.TestCase):
 	def setUp(self):
@@ -59,7 +64,7 @@ class KolibriTestingClass(unittest.TestCase):
 		# self.driver = webdriver.Chrome()
 
 	def test_a_delete_class(self):
-		self.driver.get("http://localhost:8080")
+		self.driver.get("http://localhost:8008")
 		time.sleep(3)
 		url = self.driver.current_url
 		if "setup_wizard" in url:
@@ -96,7 +101,7 @@ class KolibriTestingFacility(unittest.TestCase):
 		#self.driver = webdriver.Chrome()
 
 	def test_a_check_edit_username_facility(self):
-		self.driver.get("http://localhost:8080")
+		self.driver.get("http://localhost:8008")
 		LoginDifferentKindOfUser(self.driver, "admin","password")
 		time.sleep(2)
 		self.driver.find_element_by_xpath('//a[contains(@href,"#/facilities")]').click()
@@ -109,11 +114,7 @@ class KolibriTestingFacility(unittest.TestCase):
 		SignOut(self.driver)
 		time.sleep(4)
 		try:
-			username = getText("Enter learner username which is exist")
-			username = validate(username, "Enter learner username which is exist")
-			password = getText("Enter learner password")
-			password = validate(password, "Enter learner password")
-			LoginDifferentKindOfUser(self.driver, username, password)
+			LoginDifferentKindOfUser(self.driver, user[3].keys()[0], user[3].values()[0])
 			time.sleep(5)
 			self.driver.find_element_by_xpath("//button[@type='submit']").click()
 			time.sleep(2)
@@ -123,25 +124,14 @@ class KolibriTestingFacility(unittest.TestCase):
 			time.sleep(4)
 		except Exception:
 			print("Please enter username which is exist")
-			username = getText("Enter learner username which is exist")
-			username = validate(username, "Enter learner username which is exist")
-			password = getText("Enter learner password")
-			password = validate(password, "Enter learner password")
-			LoginDifferentKindOfUser(self.driver, username, password)
-			time.sleep(5)
-			self.driver.find_element_by_xpath("//button[@type='submit']").click()
-			time.sleep(2)
-			self.driver.find_element_by_xpath('//span[contains(text(), "menu")]').click()
-			time.sleep(4)
-			self.driver.find_element_by_xpath('//div[contains(text(), "Profile")]').click()
-		username = getText("Enter new username which is not exist")
-		username = validate(username, "Enter new username which is not exist")
+
 		self.driver.find_element_by_xpath("//input[@type='text']").clear()
 		time.sleep(3)
-		self.driver.find_element_by_xpath("//input[@type='text']").send_keys(username)
+		self.driver.find_element_by_xpath("//input[@type='text']").send_keys(user[4].keys(0))
 		time.sleep(3)
 		self.driver.find_element_by_xpath("//span[contains(text(), 'Save changes')]").click()
 		time.sleep(4)
+
 		if "An account with that username already exists" in self.driver.page_source:
 			username = getText("Enter new username which is not exist")
 			username = validate(username, "Enter new username which is not exist")
@@ -155,14 +145,26 @@ class KolibriTestingFacility(unittest.TestCase):
 		time.sleep(3)
 		self.driver.find_element_by_xpath("//span[contains(text(), 'exit_to_app')]").click()
 		time.sleep(5)
-		LoginDifferentKindOfUser(self.driver, username, password)
+		LoginDifferentKindOfUser(self.driver, user[4].keys(0), user[4].values(0))
 		time.sleep(8)
 		temp = self.driver.title
 		self.assertTrue(True, "Learn" in temp)
+		time.sleep(5)
+		self.driver.find_element_by_xpath("//button[@type='submit']").click()
+		time.sleep(2)
+		self.driver.find_element_by_xpath('//span[contains(text(), "menu")]').click()
+		time.sleep(4)
+		self.driver.find_element_by_xpath('//div[contains(text(), "Profile")]').click()
+		time.sleep(4)
+		self.driver.find_element_by_xpath("//input[@type='text']").send_keys(user[3].keys(0))
+		time.sleep(3)
+		self.driver.find_element_by_xpath("//span[contains(text(), 'Save changes')]").click()
+		time.sleep(4)
+
 		print("Edit username facility working properly")
 
 	def test_b_check_edit_fullname_facility(self):
-		self.driver.get("http://localhost:8080")
+		self.driver.get("http://localhost:8008")
 		LoginDifferentKindOfUser(self.driver, "admin","password")
 		time.sleep(2)
 		self.driver.find_element_by_xpath('//a[contains(@href,"#/facilities")]').click()
@@ -231,12 +233,9 @@ class KolibriTestingGroup(unittest.TestCase):
 		#self.driver = webdriver.Chrome()
 
 	def test_a_create_group(self):
-		self.driver.get("http://localhost:8080")
-		username = getText("Enter username of admin or coach")
-		username = validate(username, "Enter username of admin or coach")
-		password = getText("Enter password")
-		password = validate(password, "Enter password")
-		LoginDifferentKindOfUser(self.driver, username, password)
+		self.driver.get("http://localhost:8008")
+		time.sleep(3)
+		LoginDifferentKindOfUser(self.driver, user[2].keys()[0], user[2].values()[0])
 		time.sleep(7)
 		self.driver.find_element_by_xpath("//span[contains(text(), 'menu')]").click()
 		time.sleep(2)
@@ -296,12 +295,9 @@ class KolibriTestingGroup(unittest.TestCase):
 
 
 	def test_b_delete_group(self):
-		self.driver.get("http://localhost:8080")
-		username = getText("Enter username of admin or coach")
-		username = validate(username, "Enter username of admin or coach")
-		password = getText("Enter password")
-		password = validate(password, "Enter password")
-		LoginDifferentKindOfUser(self.driver, username, password)
+		self.driver.get("http://localhost:8008")
+		time.sleep(3)
+		LoginDifferentKindOfUser(self.driver, user[1].keys()[0], user[1].values()[0])
 		time.sleep(7)
 		self.driver.find_element_by_xpath("//span[contains(text(), 'menu')]").click()
 		time.sleep(2)
@@ -368,12 +364,9 @@ class KolibriTestingExam(unittest.TestCase):
 		self.driver = webdriver.Firefox()
 
 	def test_a_create_exam(self):
-		self.driver.get("http://localhost:8080")
-		username = getText("Enter username of admin or coach")
-		username = validate(username, "Enter username of admin or coach")
-		password = getText("Enter password")
-		password = validate(password, "Enter password")
-		LoginDifferentKindOfUser(self.driver, username, password)
+		self.driver.get("http://localhost:8008")
+		time.sleep(3)
+		LoginDifferentKindOfUser(self.driver, user[1].keys()[0], user[1].values()[0])
 		time.sleep(7)
 		self.driver.find_element_by_xpath("//span[contains(text(), 'menu')]").click()
 		time.sleep(2)
@@ -451,8 +444,37 @@ class KolibriTestingExam(unittest.TestCase):
 							self.driver.find_element_by_xpath("//span[contains(text(), 'Activate')]").click()
 							time.sleep(5)
 							SignOut(self.driver)
+							time.sleep(3)
 							self.assertEqual(True, True)
 							print("Exam Created and Assigned")
+							time.sleep(5)
+							LoginDifferentKindOfUser(self.driver, user[3].keys()[0], user[3].values()[0] )
+							time.sleep(6)
+							self.driver.find_element_by_xpath("//span[contains(text(), 'assignment_late')]").click()
+							time.sleep(3)
+							temp = self.driver.find_elements_by_css_selector(".exam-row")
+							# print(temp)
+							count = 0
+							for i in temp:
+								t = i.find_element(By.TAG_NAME, "h2").text
+								print(t)
+								if t == exam_name:
+									count = 1
+									i.find_element(By.TAG_NAME, "span").click()
+									time.sleep(50)
+									self.driver.find_element_by_xpath("//span[contains(text(), 'Submit exam')]").click()
+									time.sleep(3)
+									t = self.driver.find_elements(By.TAG_NAME, "span")
+									for j in t:
+										if j.text == "SUBMIT EXAM":
+											time.sleep(3)
+											j.click()
+									time.sleep(7)
+									print("Exam Given successfully")
+									self.assertEqual(True, True)
+									break
+							if count == 0:
+								print("Exam is not given for this learner")
 							break
 
 				except Exception:
@@ -466,12 +488,9 @@ class KolibriTestingExam(unittest.TestCase):
 
 
 	def test_b_delete_exam(self):
-		self.driver.get("http://localhost:8080")
-		username = getText("Enter username of admin or coach")
-		username = validate(username, "Enter username of admin or coach")
-		password = getText("Enter password")
-		password = validate(password, "Enter password")
-		LoginDifferentKindOfUser(self.driver, username, password)
+		self.driver.get("http://localhost:8008")
+		time.sleep(3)
+		LoginDifferentKindOfUser(self.driver, user[1].keys()[0], user[1].values()[0])
 		time.sleep(7)
 		self.driver.find_element_by_xpath("//span[contains(text(), 'menu')]").click()
 		time.sleep(2)
@@ -549,7 +568,7 @@ class KolibriTestingImportExport(unittest.TestCase):
 		self.driver = webdriver.Firefox()
 
 	def test_a_import_channel_from_internet(self):
-		self.driver.get("http://localhost:8080")
+		self.driver.get("http://localhost:8008")
 		time.sleep(7)
 		LoginDifferentKindOfUser(self.driver, "admin", "password")
 		time.sleep(8)
@@ -594,7 +613,7 @@ class KolibriTestingImportExport(unittest.TestCase):
 				print("Server is not responding")
 
 	def test_b_import_channel_from_localdrive(self):
-		self.driver.get("http://localhost:8080")
+		self.driver.get("http://localhost:8008")
 		time.sleep(7)
 		LoginDifferentKindOfUser(self.driver, "admin", "password")
 		time.sleep(8)
@@ -646,7 +665,7 @@ class KolibriTestingImportExport(unittest.TestCase):
 				print("Server is not responding")
 
 	def test_c_export_channel_to_localdrive(self):
-		self.driver.get("http://localhost:8080")
+		self.driver.get("http://localhost:8008")
 		time.sleep(7)
 		LoginDifferentKindOfUser(self.driver, "admin", "password")
 		time.sleep(8)
@@ -696,7 +715,7 @@ class KolibriTestingImportExport(unittest.TestCase):
 			print("Kolibri selenium channel is not imported")
 
 	def test_d_delete_channel(self):
-		self.driver.get("http://localhost:8080")
+		self.driver.get("http://localhost:8008")
 		time.sleep(7)
 		LoginDifferentKindOfUser(self.driver, "admin", "password")
 		time.sleep(8)
@@ -724,6 +743,175 @@ class KolibriTestingImportExport(unittest.TestCase):
 		else:
 			print("Kolibri selenium channel is not present")
 			self.assertEqual(True, True)
+
+	def tearDown(self):
+		self.driver.close()
+
+
+class KolibriTestingLearnTab(unittest.TestCase):
+	def setUp(self):
+		self.driver = webdriver.Firefox()
+
+
+	def test_a_exercise(self):
+		self.driver.get("http://localhost:8008")
+		time.sleep(3)
+		LoginDifferentKindOfUser(self.driver, user[3].keys()[0], user[3].values()[0])
+		time.sleep(7)
+		exercise = """M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm2 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"""
+		folder = """M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"""
+		video = """M21 3H3c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5a2 2 0 0 0-2-2zm0 14H3V5h18v12zm-5-6l-7 4V7z"""
+		self.driver.find_element_by_xpath("//span[contains(text(), 'folder')]").click()
+		time.sleep(4)
+		self.driver.find_element_by_xpath("//span[contains(text(), 'apps')]").click()
+		time.sleep(2)
+		text = getText("Enter channel name")
+		time.sleep(2)
+		self.driver.find_element_by_xpath("//div[contains(text(), '%s')]" %text).click()
+		time.sleep(3)
+		counter = 5
+		try:
+			for p in range(0,counter):
+				time.sleep(2)
+				count = 0
+				if exercise not in self.driver.page_source and folder not in self.driver.page_source:
+					time.sleep(3)
+					self.driver.find_element_by_xpath("//span[contains(text(), 'folder')]").click()
+					counter = counter + 5
+					time.sleep(3)
+				elif exercise not in self.driver.page_source:
+					text = getText("Enter partial text of the folder")
+					temp = self.driver.find_elements(By.TAG_NAME, "h3")
+					print("Not found")
+					for i in temp:
+						t = i.text
+						if t.find(text) != -1:
+							print(t)
+							i.click()
+							time.sleep(4)
+							break
+				elif exercise in self.driver.page_source and folder in self.driver.page_source:
+					print("Mixed found")
+					text = getText("Enter partial text")
+					temp = self.driver.find_elements(By.TAG_NAME, "h3")
+					for i in temp:
+						t = i.text
+						if t.find(text) != -1:
+							print(t)
+							time.sleep(3)
+							i.click()
+
+							if folder not in self.driver.page_source and exercise not in self.driver.page_source and video not in self.driver.page_source:
+								count = 1
+								print(t)
+								time.sleep(100)
+
+								break
+							else:
+								time.sleep(6)
+				else:
+					print("found")
+					text = getText("Enter partial text")
+					temp = self.driver.find_elements(By.TAG_NAME, "h3")
+					for i in temp:
+						t = i.text
+						if t.find(text) != -1:
+								count = 1
+								print(t)
+								time.sleep(3)
+								i.click()
+								time.sleep(100)
+								break
+				if count == 1:
+					self.assertEqual(True, True)
+					break
+		except Exception:
+			print("Something going wrong")
+
+
+	def test_b_video(self):
+		self.driver.get("http://localhost:8008")
+		time.sleep(3)
+		LoginDifferentKindOfUser(self.driver, user[3].keys()[0], user[3].values()[0])
+		time.sleep(7)
+		exercise = """M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 0c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1zm2 14H7v-2h7v2zm3-4H7v-2h10v2zm0-4H7V7h10v2z"""
+		folder = """M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"""
+		video = """M21 3H3c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5a2 2 0 0 0-2-2zm0 14H3V5h18v12zm-5-6l-7 4V7z"""
+		self.driver.find_element_by_xpath("//span[contains(text(), 'folder')]").click()
+		time.sleep(4)
+		self.driver.find_element_by_xpath("//span[contains(text(), 'apps')]").click()
+		time.sleep(2)
+		text = getText("Enter channel name")
+		time.sleep(2)
+		self.driver.find_element_by_xpath("//div[contains(text(), '%s')]" %text).click()
+		time.sleep(3)
+		counter = 5
+		try:
+			for p in range(0,counter):
+				time.sleep(2)
+				count = 0
+				if video not in self.driver.page_source and folder not in self.driver.page_source:
+					time.sleep(3)
+					self.driver.find_element_by_xpath("//span[contains(text(), 'folder')]").click()
+					counter = counter + 5
+					time.sleep(3)
+				elif video not in self.driver.page_source:
+					text = getText("Enter partial text of the folder")
+					temp = self.driver.find_elements(By.TAG_NAME, "h3")
+					print("Not found")
+					for i in temp:
+						t = i.text
+						if t.find(text) != -1:
+							print(t)
+							i.click()
+							time.sleep(4)
+							break
+				elif video in self.driver.page_source and folder in self.driver.page_source:
+					print("Mixed found")
+					text = getText("Enter partial text")
+					temp = self.driver.find_elements(By.TAG_NAME, "h3")
+					for i in temp:
+						t = i.text
+						if t.find(text) != -1:
+							print(t)
+							time.sleep(3)
+							i.click()
+
+							if folder not in self.driver.page_source and exercise not in self.driver.page_source and video not in self.driver.page_source:
+								count = 1
+								print(t)
+								self.driver.find_element_by_xpath("//span[contains(text(), 'Play')]").click()
+
+								time.sleep(240)
+
+								break
+							else:
+								time.sleep(6)
+				else:
+					print("found")
+					text = getText("Enter partial text")
+					temp = self.driver.find_elements(By.TAG_NAME, "h3")
+					for i in temp:
+						t = i.text
+						if t.find(text) != -1:
+								count = 1
+								print(t)
+								time.sleep(3)
+								i.click()
+								time.sleep(3)
+								self.driver.find_element_by_xpath("//span[contains(text(), 'Play')]").click()
+								time.sleep(240)
+								break
+				if count == 1:
+					self.assertEqual(True, True)
+					break
+
+		except Exception:
+			print("Something going wrong")
+
+
+
+
 
 	def tearDown(self):
 		self.driver.close()
